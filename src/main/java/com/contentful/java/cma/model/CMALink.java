fd7bbe40d26a2d612d5ff6dea24a256184a93644
Mario Bodemann <mario@contentful.com>
2017-04-11 11:51:54 +0200

    Add more convenient setters on CMALink and CMAUpload.
    This eases the use of setters in a chain, since no casting is needed.

--- src/main/java/com/contentful/java/cma/model/CMALink.java
+++ src/main/java/com/contentful/java/cma/model/CMALink.java
@@ -25,4 +25,48 @@ public class CMALink extends CMAResource {
     super(CMAType.Link);
     system.setLinkType(linkedType);
   }
+
+  /**
+   * Change ths system property to a new one.
+   *
+   * @param system the system property to be set.
+   * @return this instance of a link for chaining.
+   */
+  @SuppressWarnings("unchecked")
+  @Override public CMALink setSystem(CMASystem system) {
+    return super.setSystem(system);
+  }
+
+  /**
+   * Set this ids value.
+   *
+   * @param id to be set.
+   * @return This instance for chaining.
+   */
+  @SuppressWarnings("unchecked")
+  @Override public CMALink setId(String id) {
+    return super.setId(id);
+  }
+
+  /**
+   * Change this instances version number.
+   *
+   * @param version the version number to be set.
+   * @return this instance to be chained.
+   */
+  @SuppressWarnings("unchecked")
+  @Override public CMALink setVersion(Integer version) {
+    return super.setVersion(version);
+  }
+
+  /**
+   * Set the id of the space this link.
+   *
+   * @param spaceId the id to be set.
+   * @return this instance for ease of chaining.
+   */
+  @SuppressWarnings("unchecked")
+  @Override public CMALink setSpaceId(String spaceId) {
+    return super.setSpaceId(spaceId);
+  }
 }
--- src/main/java/com/contentful/java/cma/model/CMAUpload.java
+++ src/main/java/com/contentful/java/cma/model/CMAUpload.java
@@ -10,4 +10,48 @@ public class CMAUpload extends CMAResource {
   public CMAUpload() {
     super(CMAType.Upload);
   }
+
+  /**
+   * Update the current instances system property to a complete new one.
+   *
+   * @param system sets the system property.
+   * @return this instance for chaining.
+   */
+  @SuppressWarnings("unchecked")
+  @Override public CMAUpload setSystem(CMASystem system) {
+    return super.setSystem(system);
+  }
+
+  /**
+   * Sets this resources id.
+   *
+   * @param id to be set
+   * @return this instance for chaining.
+   */
+  @SuppressWarnings("unchecked")
+  @Override public CMAUpload setId(String id) {
+    return super.setId(id);
+  }
+
+  /**
+   * Sets the version of this resource.
+   *
+   * @param version the version number to be set.
+   * @return this instance of CMAUpload for chaining.
+   */
+  @SuppressWarnings("unchecked")
+  @Override public CMAUpload setVersion(Integer version) {
+    return super.setVersion(version);
+  }
+
+  /**
+   * Set the space id in a chainable manner.
+   *
+   * @param spaceId the id to be set.
+   * @return this instance for chaining.
+   */
+  @SuppressWarnings("unchecked")
+  @Override public CMAUpload setSpaceId(String spaceId) {
+    return super.setSpaceId(spaceId);
+  }
 }
