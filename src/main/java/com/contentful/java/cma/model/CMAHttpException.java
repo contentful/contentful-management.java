594d69962f857eade03d0aefd101092b9905441b
Mario Bodemann <mario@contentful.com>
2018-02-14 14:09:07 +0100

    Add details to error messages
    closing #98

--- src/main/java/com/contentful/java/cma/model/CMAHttpException.java
+++ src/main/java/com/contentful/java/cma/model/CMAHttpException.java
@@ -1,7 +1,9 @@
 package com.contentful.java.cma.model;
 
 import com.contentful.java.cma.model.RateLimits.DefaultParser;
+import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -16,10 +18,212 @@ import static java.lang.String.format;
  * This class will represent known Contentful exceptions
  */
 public class CMAHttpException extends RuntimeException {
+  /**
+   * Error body potentially delivered with an error request.
+   */
+  public static class ErrorBody {
+    /**
+     * System properties of an error body
+     */
+    public static class Sys {
+      String type;
+      String id;
+
+      /**
+       * @return the type, which should be `Error`.
+       */
+      public String getType() {
+        return type;
+      }
+
+      /**
+       * @return the id of the error.
+       */
+      public String getId() {
+        return id;
+      }
+
+      /**
+       * @return a human readable string, representing the object.
+       */
+      @Override public String toString() {
+        return "Sys { "
+            + (getId() != null ? "id = " + getId() + ", " : "")
+            + (getType() != null ? "type = " + getType() + " " : "")
+            + "}";
+      }
+    }
+
+    /**
+     * Class to describe the error details.
+     */
+    public static class Details {
+      /**
+       * Which error did actually happen where?
+       */
+      public static class Error {
+        String name;
+        String details;
+        String type;
+        String filter;
+        String value;
+        Object path;
+
+        /**
+         * @return the name of this error.
+         */
+        public String getName() {
+          return name;
+        }
+
+        /**
+         * @return a detailed description of the error.
+         */
+        public String getDetails() {
+          return details;
+        }
+
+        /**
+         * @return the type of this error.
+         */
+        public String getType() {
+          return type;
+        }
+
+        /**
+         * @return the filter this error produced.
+         */
+        public String getFilter() {
+          return filter;
+        }
+
+        /**
+         * @return the value triggering this error.
+         */
+        public String getValue() {
+          return value;
+        }
+
+        /**
+         * @return a path contributing to this error.
+         */
+        public Object getPath() {
+          return path;
+        }
+
+        /**
+         * @return a human readable string, representing the object.
+         */
+        @Override public String toString() {
+          return "Error { "
+              + (getDetails() != null ? "details = " + getDetails() + ", " : "")
+              + (getFilter() != null ? "filter = " + getFilter() + ", " : "")
+              + (getName() != null ? "name = " + getName() + ", " : "")
+              + (getPath() != null ? "path = " + getPath() + ", " : "")
+              + (getType() != null ? "type = " + getType() + ", " : "")
+              + (getValue() != null ? "value = " + getValue() + " " : "")
+              + "}";
+        }
+      }
+
+      String type;
+      String space;
+      List<Error> errors;
+      List<String> keys;
+
+      /**
+       * @return the type of this detail.
+       */
+      public String getType() {
+        return type;
+      }
+
+      /**
+       * @return the space given to the error.
+       */
+      public String getSpace() {
+        return space;
+      }
+
+      /**
+       * @return the list of errors encountered
+       */
+      public List<Error> getErrors() {
+        return errors;
+      }
+
+      /**
+       * @return a list of keys contributing to this error.
+       */
+      public List<String> getKeys() {
+        return keys;
+      }
+
+      /**
+       * @return a human readable string, representing the object.
+       */
+      @Override public String toString() {
+        return "Details { "
+            + (getErrors() != null ? "errors = " + getErrors() + ", " : "")
+            + (getSpace() != null ? "space = " + getSpace() + ", " : "")
+            + (getKeys() != null ? "keys = " + getKeys() + ", " : "")
+            + (getType() != null ? "type = " + getType() + " " : "")
+            + "}";
+      }
+    }
+
+    Sys sys;
+    String message;
+    String requestId;
+    Details details;
+
+    /**
+     * @return the sys of this response, containing the error id.
+     */
+    public Sys getSys() {
+      return sys;
+    }
+
+    /**
+     * @return the message this error contained.
+     */
+    public String getMessage() {
+      return message;
+    }
+
+    /**
+     * @return the request id used to help the Contentful staff.
+     */
+    public String getRequestId() {
+      return requestId;
+    }
+
+    /**
+     * @return more information about the error.
+     */
+    public Details getDetails() {
+      return details;
+    }
+
+    /**
+     * @return a human readable string, representing the object.
+     */
+    @Override public String toString() {
+      return "ErrorBody { "
+          + (getDetails() != null ? "details = " + getDetails() + ", " : "")
+          + (getMessage() != null ? "message = " + getMessage() + ", " : "")
+          + (getRequestId() != null ? "requestId = " + getRequestId() + ", " : "")
+          + (getSys() != null ? "sys = " + getSys() + " " : "")
+          + "}";
+    }
+  }
+
   private final Request request;
   private final Response response;
 
   private final RateLimits ratelimits;
+
+  private ErrorBody errorBody;
 
   /**
    * Construct an error response.
@@ -35,6 +239,13 @@ public class CMAHttpException extends RuntimeException {
     this.request = request;
     this.response = response;
 
+    try {
+      final String body = response.body() != null ? response.body().string() : null;
+      this.errorBody = new GsonBuilder().create().fromJson(body, ErrorBody.class);
+    } catch (IOException e) {
+      this.errorBody = null;
+    }
+
     final Map<String, List<String>> headers = response.headers().toMultimap();
     this.ratelimits = new DefaultParser().parse(headers);
   }
@@ -46,13 +257,24 @@ public class CMAHttpException extends RuntimeException {
    */
   @Override
   public String toString() {
-    return format(
-        Locale.getDefault(),
-        "FAILED REQUEST:\n\t%s\n\t↳ Header{%s}\n\t%s\n\t↳ Header{%s}",
-        request.toString(),
-        headersToString(request.headers()),
-        response.toString(),
-        headersToString(response.headers()));
+    if (errorBody == null) {
+      return format(
+          Locale.getDefault(),
+          "FAILED \n\t%s\n\t↳ Header{%s}\n\t%s\n\t↳ Header{%s}",
+          request.toString(),
+          headersToString(request.headers()),
+          response.toString(),
+          headersToString(response.headers()));
+    } else {
+      return format(
+          Locale.getDefault(),
+          "FAILED %s\n\t%s\n\t↳ Header{%s}\n\t%s\n\t↳ Header{%s}",
+          errorBody.toString(),
+          request.toString(),
+          headersToString(request.headers()),
+          response.toString(),
+          headersToString(response.headers()));
+    }
   }
 
   /**
@@ -102,6 +324,13 @@ public class CMAHttpException extends RuntimeException {
    */
   public int rateLimitReset() {
     return ratelimits.getReset();
+  }
+
+  /**
+   * @return a modeled error body response.
+   */
+  public ErrorBody getErrorBody() {
+    return errorBody;
   }
 
   private String headersToString(Headers headers) {
---
+++ src/test/kotlin/com/contentful/java/cma/CMAHttpExceptionTest.kt
@@ -0,0 +1,232 @@
+package com.contentful.java.cma
+
+import com.contentful.java.cma.lib.TestUtils
+import com.contentful.java.cma.model.CMAHttpException
+import okhttp3.*
+import kotlin.test.assertEquals
+import org.junit.Test as test
+
+class CMAHttpExceptionTest : BaseTest() {
+    val HEADER_RATE_LIMIT_HOUR_LIMIT = "X-Contentful-RateLimit-Hour-Limit"
+    val HEADER_RATE_LIMIT_HOUR_REMAINING = "X-Contentful-RateLimit-Hour-Remaining"
+    val HEADER_RATE_LIMIT_SECOND_LIMIT = "X-Contentful-RateLimit-Second-Limit"
+    val HEADER_RATE_LIMIT_SECOND_REMAINING = "X-Contentful-RateLimit-Second-Remaining"
+    val HEADER_RATE_LIMIT_RESET = "X-Contentful-RateLimit-Reset"
+
+    @test
+    fun testHeadersParsing() {
+        val request = Request.Builder().url("https://example.com/foo").build()
+
+        val response = buildMockedResponse(request, "{\"sys\":{\"id\":\"manualid\"}}")
+
+        val e = CMAHttpException(request, response)
+
+        assertEquals("message", e.responseMessage())
+
+        assertEquals(1, e.rateLimitHourLimit())
+        assertEquals(2, e.rateLimitHourRemaining())
+        assertEquals(3, e.rateLimitSecondLimit())
+        assertEquals(4, e.rateLimitSecondRemaining())
+
+        assertEquals(-1, e.rateLimitReset(), "Error while parsing number.")
+
+        assertEquals(500, e.responseCode())
+
+        assertEquals("FAILED ErrorBody { sys = Sys { id = manualid, } }\n" +
+                "\tRequest{method=GET, url=https://example.com/foo, tag=null}\n" +
+                "\t↳ Header{}\n" +
+                "\tResponse{" +
+                "protocol=http/1.1, code=500, message=message, url=https://example.com/foo}\n" +
+                "\t↳ Header{X-Contentful-RateLimit-Hour-Limit: 1, " +
+                "X-Contentful-RateLimit-Hour-Remaining: 2, X-Contentful-RateLimit-Reset: 5k, " +
+                "X-Contentful-RateLimit-Second-Limit: 3, " +
+                "X-Contentful-RateLimit-Second-Remaining: 4}",
+                e.toString())
+    }
+
+    @test
+    fun testUnknownPath() {
+        val request = Request.Builder().url("https://example.com/foo").build()
+
+        val response = buildMockedResponse(request, TestUtils.fileToString("error_unknown_path.json"))
+
+        val e = CMAHttpException(request, response).errorBody
+
+        assertEquals(
+                "The query you sent was invalid. Probably a filter or ordering specification is not applicable to the type of a field.",
+                e.message)
+
+        assertEquals("InvalidQuery", e.sys.id)
+        assertEquals("f5841eb3c46dc4a2c4f67c2d5a027f1c", e.requestId)
+
+        assertEquals(1, e.details.errors.size)
+        assertEquals("unknown", e.details.errors[0].name)
+        val path = e.details.errors[0].path as List<*>
+        assertEquals("sys", path[0])
+        assertEquals("foo", path[1])
+        assertEquals("The path \"sys.foo\" is not recognized", e.details.errors[0].details)
+        assertEquals("f5841eb3c46dc4a2c4f67c2d5a027f1c", e.requestId)
+    }
+
+    @test
+    fun testWrongTypeForLimit() {
+        val request = Request.Builder().url("https://example.com/foo").build()
+
+        val response = buildMockedResponse(request, TestUtils.fileToString("error_wrong_type_for_limit.json"))
+
+        val e = CMAHttpException(request, response).errorBody
+
+        assertEquals("InvalidQuery", e.sys.id)
+        assertEquals("20add8f6cab860a61752c2e0b00966f1", e.requestId)
+
+        assertEquals("The value provided for \"limit\" is invalid. Please provide a valid number value", e.message)
+    }
+
+    @test
+    fun testWrongTypeForDate() {
+        val request = Request.Builder().url("https://example.com/foo").build()
+
+        val response = buildMockedResponse(request, TestUtils.fileToString("error_wrong_type_for_date.json"))
+
+        val e = CMAHttpException(request, response).errorBody
+
+        assertEquals("InvalidQuery", e.sys.id)
+        assertEquals("4dfcab3bd85b753033280b1fa38d7717", e.requestId)
+
+        assertEquals(
+                "The query you sent was invalid. Probably a filter or ordering specification is not applicable to the type of a field.",
+                e.message)
+
+        assertEquals(1, e.details.errors.size)
+
+        assertEquals("value", e.details.errors[0].name)
+        val path = e.details.errors[0].path as List<*>
+        assertEquals("sys", path[0])
+        assertEquals("archivedAt", path[1])
+        assertEquals("Date", e.details.errors[0].type)
+        assertEquals("equals", e.details.errors[0].filter)
+        assertEquals("false used on sys.archivedAt is not a valid value for type Date.", e.details.errors[0].details)
+    }
+
+    @test
+    fun testPostWithWrongData() {
+        val request = Request.Builder().url("https://example.com/foo").build()
+
+        val response = buildMockedResponse(request, TestUtils.fileToString("error_post_wrong_data.json"))
+
+        val e = CMAHttpException(request, response).errorBody
+
+        assertEquals("InvalidEntry", e.sys.id)
+        assertEquals("0cb77b0bf64d814ed182a668be4dc0eb", e.requestId)
+
+        assertEquals("Validation error", e.message)
+
+        assertEquals(1, e.details.errors.size)
+
+        assertEquals("unknown", e.details.errors[0].name)
+        val path = e.details.errors[0].path as List<*>
+        assertEquals("fields", path[0])
+        assertEquals("test", path[1])
+        assertEquals("bar", e.details.errors[0].value)
+        assertEquals("The property \"test\" is not allowed here.", e.details.errors[0].details)
+    }
+
+    @test
+    fun testInvalidJson() {
+        val request = Request.Builder().url("https://example.com/foo").build()
+
+        val response = buildMockedResponse(request, TestUtils.fileToString("error_invalid_json.json"))
+
+        val e = CMAHttpException(request, response).errorBody
+
+        assertEquals("BadRequest", e.sys.id)
+        assertEquals("8467e3d93f5650aefc596367370b6fa3", e.requestId)
+
+        assertEquals(
+                "Invalid JSON in request body: {\n\t\"fields\": {\n\t\t\"test\": {\n\t\t\t\"en-US: \"bar\"\n\t\t}\n\t}\n}",
+                e.message)
+    }
+
+    @test
+    fun testWrongType() {
+        val request = Request.Builder().url("https://example.com/foo").build()
+
+        val response = buildMockedResponse(request, TestUtils.fileToString("error_wrong_type.json"))
+
+        val e = CMAHttpException(request, response).errorBody
+
+        assertEquals("InvalidEntry", e.sys.id)
+        assertEquals("8cd7ef1751dd283ee230e6a58efef8ec", e.requestId)
+
+        assertEquals("Validation error", e.message)
+
+        assertEquals(1, e.details.errors.size)
+
+        assertEquals("type", e.details.errors[0].name)
+        val path = e.details.errors[0].path as List<*>
+        assertEquals("fields", path[0])
+        assertEquals("xqZFfK7YdzEfhhJg", path[1])
+        assertEquals("en-US", path[2])
+        assertEquals("bar", e.details.errors[0].value)
+        assertEquals("Object", e.details.errors[0].type)
+        assertEquals("The type of \"value\" is incorrect, expected type: Object", e.details.errors[0].details)
+    }
+
+    @test
+    fun testWrongField() {
+        val request = Request.Builder().url("https://example.com/foo").build()
+
+        val response = buildMockedResponse(request, TestUtils.fileToString("error_wrong_field.json"))
+
+        val e = CMAHttpException(request, response).errorBody
+
+        assertEquals("InvalidEntry", e.sys.id)
+        assertEquals("fieldsRequestId", e.requestId)
+
+        assertEquals("The body you sent contains an unknown key.", e.message)
+
+        assertEquals(1, e.details.keys.size)
+        assertEquals("a_field_that_does_not_exist", e.details.keys[0])
+    }
+
+    @test
+    fun testToString() {
+        val request = Request.Builder().url("https://example.com/foo").build()
+
+        val response = buildMockedResponse(request, TestUtils.fileToString("error_wrong_type.json"))
+
+        val e = CMAHttpException(request, response)
+
+        assertEquals("FAILED ErrorBody { details = Details { errors = [Error " +
+                "{ details = The type of \"value\" is incorrect, expected type: Object," +
+                " name = type, path = [fields, xqZFfK7YdzEfhhJg, en-US], type = Object, " +
+                "value = bar }], }, message = Validation error, " +
+                "requestId = 8cd7ef1751dd283ee230e6a58efef8ec, sys = Sys { id = InvalidEntry, " +
+                "type = Error } }\n" +
+                "\tRequest{method=GET, url=https://example.com/foo, tag=null}\n" +
+                "\t↳ Header{}\n" +
+                "\tResponse{protocol=http/1.1, code=500, message=message, " +
+                "url=https://example.com/foo}\n" +
+                "\t↳ Header{X-Contentful-RateLimit-Hour-Limit: 1, " +
+                "X-Contentful-RateLimit-Hour-Remaining: 2, X-Contentful-RateLimit-Reset: 5k, " +
+                "X-Contentful-RateLimit-Second-Limit: 3, " +
+                "X-Contentful-RateLimit-Second-Remaining: 4}", e.toString())
+    }
+
+    private fun buildMockedResponse(request: Request?, body: String): Response? {
+        return Response.Builder()
+                .request(request)
+                .body(
+                        ResponseBody.create(MediaType.parse("application/json"), body)
+                )
+                .header(HEADER_RATE_LIMIT_HOUR_LIMIT, "1")
+                .header(HEADER_RATE_LIMIT_HOUR_REMAINING, "2")
+                .header(HEADER_RATE_LIMIT_SECOND_LIMIT, "3")
+                .header(HEADER_RATE_LIMIT_SECOND_REMAINING, "4")
+                .header(HEADER_RATE_LIMIT_RESET, "5k")
+                .protocol(Protocol.HTTP_1_1)
+                .code(500)
+                .message("message")
+                .build()
+    }
+}
