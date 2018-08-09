3876cc3215952da81ac0f3435d8536d9b3fa6af9
Mario Bodemann <mario@contentful.com>
2018-08-09 16:52:21 +0200

    test entries on master, not on the given environment

--- src/test/kotlin/com/contentful/java/cma/e2e/EntryE2E.kt
+++ src/test/kotlin/com/contentful/java/cma/e2e/EntryE2E.kt
@@ -16,15 +16,34 @@
 
 package com.contentful.java.cma.e2e
 
+import com.contentful.java.cma.CMAClient
 import com.contentful.java.cma.model.CMAEntry
+import org.junit.BeforeClass
 import org.junit.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 open class EntryE2E : Base() {
+    companion object {
+        @BeforeClass
+        @JvmStatic
+        fun setUpEntriesSuite() {
+            client = CMAClient.Builder().apply {
+                if (!(PROXY.isNullOrEmpty())) {
+                    setCoreEndpoint(PROXY)
+                    setUploadEndpoint(PROXY)
+                }
+                setAccessToken(ACCESS_TOKEN)
+                setSpaceId(SPACE_ID)
+                setCoreCallFactory(createCallFactory())
+                setUploadCallFactory(createCallFactory())
+            }.build()
+        }
+    }
+
     @Test
-    fun testCreateEntry() {
+    fun testEntries() {
         var entry = CMAEntry()
         entry.setField("title", "en-US", "My Title")
 
@@ -34,6 +53,12 @@ open class EntryE2E : Base() {
         assertFalse(entry.isPublished)
         entry = client.entries().publish(entry)
         assertTrue(entry.isPublished)
+
+        val snapshots = client.entries().fetchAllSnapshots(entry)
+        assertEquals(1, snapshots.items.size)
+
+        val snapshot = client.entries().fetchOneSnapshot(entry, snapshots.items.first().id)
+        assertEquals(snapshots.items.first().toString().trim(), snapshot.toString().trim())
 
         entry = client.entries().unPublish(entry)
         assertFalse(entry.isPublished)
@@ -53,5 +78,7 @@ open class EntryE2E : Base() {
         assertFalse(entry.isArchived)
 
         assertEquals(204, client.entries().delete(entry))
+
+        assertEquals(3, client.entries().fetchAll().total)
     }
 }
