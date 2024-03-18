0462dc44ca9f231b9754987d758f237b6145ae5b
bkljuno <bkljuno@dccsintra.net>
2024-03-18 23:20:28 +0100

    Init: Update EntrySerializer to include metadata in a payload

--- src/main/java/com/contentful/java/cma/gson/MetadataSerializer.java
+++ src/main/java/com/contentful/java/cma/gson/MetadataSerializer.java
@@ -4,7 +4,11 @@ import com.contentful.java.cma.model.CMAMetadata;
 import com.contentful.java.cma.model.CMASystem;
 import com.contentful.java.cma.model.CMATag;
 import com.contentful.java.cma.model.CMAType;
-import com.google.gson.*;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 
 import java.lang.reflect.Type;
 import java.util.List;
@@ -18,11 +22,11 @@ public class MetadataSerializer implements JsonSerializer<CMAMetadata> {
     }
 
     private JsonArray serializeTags(List<CMATag> tags) {
-        final JsonArray jsonArray = new JsonArray();
-        for (final CMATag tag : tags) {
-            final JsonObject tagObject = new JsonObject();
-            final JsonObject sysObject = new JsonObject();
-            final CMASystem system = tag.getSystem();
+        JsonArray jsonArray = new JsonArray();
+        for (CMATag tag : tags) {
+            JsonObject tagObject = new JsonObject();
+            JsonObject sysObject = new JsonObject();
+            CMASystem system = tag.getSystem();
 
             sysObject.addProperty("type", CMAType.Link.toString());
             sysObject.addProperty("linkType", CMAType.Tag.toString());
