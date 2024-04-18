434b85ee16a7c5c32c18e1b5becb2b086eeaf613
Rafal Niski <rafalniski@gmail.com>
2024-04-18 16:14:14 +0200

    Add support for rich text tables (#176)

---
+++ src/main/java/com/contentful/java/cma/model/rich/CMARichTableCell.java
@@ -0,0 +1,13 @@
+package com.contentful.java.cma.model.rich;
+
+/**
+ * Rich Text table cell.
+ */
+public class CMARichTableCell extends CMARichBlock {
+  /**
+   * Create a table.
+   */
+  public CMARichTableCell() {
+    super("table-cell");
+  }
+}
---
+++ src/main/java/com/contentful/java/cma/model/rich/CMARichTableRow.java
@@ -0,0 +1,13 @@
+package com.contentful.java.cma.model.rich;
+
+/**
+ * Rich Text table row.
+ */
+public class CMARichTableRow extends CMARichBlock {
+  /**
+   * Create a table row.
+   */
+  public CMARichTableRow() {
+    super("table-row");
+  }
+}
